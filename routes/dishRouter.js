cc23ca2f46670f5f611728f5ae32862e4c7f7981
ddevansh160 <ddevansh160@gmail.com>
2020-07-18 10:59:51 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -5,46 +5,75 @@ const dishRouter = express.Router();
 dishRouter.use(bodyparser.json());
 
 dishRouter.route('/')
-.all((req,res,next) => { 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-  
-    res.end('Will send all dishses to you!');
+    Dishes.find({})
+    .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-.post((req,res,next) => {
-    // Use POST to add a new resource
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-  }) 
+.post((req, res, next) => {
+    Dishes.create(req.body)
+    .then((dish) => {
+        console.log('Dish Created ', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+}) 
 .put((req,res,next) => { 
       // Use PUT when you want to modify a singular resource which is already a part of resources 
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-.delete((req,res,next) => {
-    res.end('Deleting all dishes');
+.delete((req, res, next) => {
+    Dishes.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));    
 });
 
 
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
-    res.end('Will send details of the dish' +req.params.dishId+ " to you ");
+    Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req,res,next) => {
       // Use POST to add a new resource
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put((req,res,next) => { 
-        // Use PUT when you want to modify a singular resource which is already a part of resources 
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-          ' with details: ' + req.body.description);
-  })
-.delete((req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
+.put((req, res, next) => {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, { new: true })
+    .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
   
 
